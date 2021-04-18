b73d353f086cccbbecf66bb824ea60ee6614f12e
mkhuselityhobeka@gmail.com <mkhuselityhobeka@gmail.com>
2021-04-18 14:26:57 +0200

    added mornitoring package and class to mornitor method exectution time,updated the prometheus.yml

---
+++ src/main/java/com/active/store/monitoring/MornitoringMethodExecutionTime.java
@@ -0,0 +1,21 @@
+package com.active.store.monitoring;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MornitoringMethodExecutionTime {
+
+    @Configuration
+    public class MonitorMethodExecutionTime {
+
+        @Bean
+        public TimedAspect timedAspect(MeterRegistry meterRegistry){
+            return new TimedAspect (meterRegistry);
+
+        }
+    }
+
+}
